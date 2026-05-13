b1b0b7c415a964117c7a8e9c71c520cc3446badb
salohiddin27 <salohiddinnurbayev25@gmail.com>
2026-05-13 16:53:17 +0500

    fix static files

--- root/settings.py
+++ root/settings.py
@@ -103,3 +103,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
