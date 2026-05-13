46cf1bf9bb584fca68a2439597f3d79686ddfe57
salohiddin27 <salohiddinnurbayev25@gmail.com>
2026-05-13 17:15:27 +0500

    update password

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@gmail.com', 'admin123')" | python manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.filter(username='salohiddin').exists() or User.objects.create_superuser('salohiddin', 'admin@gmail.com', '1')" | python manage.py shell
