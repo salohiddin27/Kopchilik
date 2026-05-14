549598bf9d616ee18a80f860fc741cf78e195977
salohiddin27 <salohiddinnurbayev25@gmail.com>
2026-05-14 14:19:50 +0500

    models va settings yangilandi

--- root/urls.py
+++ root/urls.py
@@ -1,8 +1,11 @@
-
+# root/urls.py
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
+    path('api/', include('main.urls')),
+    path('api/token/', TokenObtainPairView.as_view()),      # ✅ login → token
+    path('api/token/refresh/', TokenRefreshView.as_view()), # ✅ token yangilash
 ]
